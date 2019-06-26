2d98c92d5da2bc18bd80036502443314c8f46c19
hanmeimei <hmm@hanmeimeideMacBook-Pro.local>
2019-06-27 02:00:34 +0800

    add

--- assignDemo.js
+++ assignDemo.js
@@ -14,5 +14,18 @@ function myAssign(target, ...source) {
 var a = {'a': 1};
 var b = {'b': 2, 'c': 3};
 var c = {'c': 4, 'd': 5};
-myAssign(a, b, c);
-console.log(JSON.stringify(a));
+// myAssign(a, b, c);
+// console.log(JSON.stringify(a));
+
+let A = {_v: 111};
+
+Object.defineProperty(A, 'v', {
+    get () {
+        return this._v;
+    },
+    set (newValue) {
+        this._v = newValue;
+    }
+});
+
+console.log(A.v);
