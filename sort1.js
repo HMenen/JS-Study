502b336bb19731e8e22de49a65a321d527862581
hanmeimei <hmm@hanmeimeideMacBook-Pro.local>
2019-06-30 22:08:33 +0800

    add

--- sort1.js
+++ sort1.js
@@ -127,5 +127,38 @@ function swap (arr, i, j) {
     arr[j] = temp;
 }
 
-heapSort(arr);
-console.log(arr)
+// heapSort(arr);
+// console.log(arr)
+
+// [3, 1, 1, 2, 5];
+function mergeSort (arr) {
+    if (arr.length == 1) return arr
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid, arr.length + 1);
+    return merge(mergeSort(left), mergeSort(right));
+}
+function merge(left, right) {
+    let result = [];
+    let l = 0;
+    let r = 0;
+    while (l < left.length && r < right.length) {
+        if (left[l] <= right[r]) {
+            result.push(left[l]);
+            l++;
+        } else {
+            result.push(right[r]);
+            r++;
+        }
+    }
+    while (l < left.length) {
+        result.push(left[l]);
+        l++;
+    }
+    while (r < right.length) {
+        result.push(right[r]);
+        r++;
+    }
+    return result;
+}
+console.log(mergeSort(arr));
