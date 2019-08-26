4302e7211fa7dda2901b31db00911f725e9a2405
HMenen <844316200@qq.com>
2019-08-26 11:36:58 +0800

    add 类型判断方法

---
+++ judge-type.js
@@ -0,0 +1,43 @@
+/**
+ * js 类型判断
+ * Object.prototype.toString的使用
+ */
+
+ /**
+  * 构造类型对象，
+  * {
+  * '[Object Boolean]': 'boolean',
+    '[Object Number]': 'number',
+    '[Object String]': 'string',
+    '[Object Function]': 'function',
+    '[Object Array]': 'array',
+    '[Object Object]': 'object',
+    '[Object Date]': 'date',
+    '[Object RegExp]': 'regexp' 
+  * }
+  */
+function getClassType() {
+  let classType = {};
+  const types = "Boolean Number String Function Array Object Date RegExp";
+  types.split(" ").map((type, index) => classType['[object ' + type + ']'] = type.toLowerCase());
+  return classType;
+ }
+//  console.log(getClassType())
+/**
+ * 输入要类型判断的对象；返回类型（String）
+ * @param {object} obj 
+ */
+function type(obj) {
+  const types = getClassType();
+  if (obj === null) {
+    return obj + "";
+  }
+  if (typeof obj === "object" || typeof obj === "function" ) {
+    const type = Object.prototype.toString.call(obj);
+    return types[type]
+  } else {
+    return typeof obj;
+  }
+}
+const a = function a(){}
+console.log(type(a))
