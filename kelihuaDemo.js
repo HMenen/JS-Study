ebb404491a14664d9f3af0f2ed4a76ecb1dd9d3b
HMenen <844316200@qq.com>
2019-08-30 13:41:05 +0800

    增加一个带有遮罩层的弹框

--- kelihuaDemo.js
+++ kelihuaDemo.js
@@ -14,5 +14,24 @@ function add(a, b) {
   return a + b;
 }
 
-var addCurry = curry(add, 1, 2);
-console.log(addCurry()) //3
+// var addCurry = curry(add, 1, 2);
+// console.log(addCurry()) //3
+
+/**
+ * 柯里化函数
+ */
+function curryEnhance() {
+  const args = [].slice.call(arguments, 1);
+  const fn = arguments[0];
+  return function() {
+    let newArgs = [].slice.call(arguments).concat(args);
+    if(fn.length > newArgs.length) {
+      return curryEnhance(fn, ...newArgs);
+    } else {
+      return fn.apply(this, newArgs);
+    }
+  }
+}
+
+let ret = curryEnhance(add)(2)(1);
+console.log(ret)
