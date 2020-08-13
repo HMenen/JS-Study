53b07148553443e484a048c3792fad13801dbc55
HMenen <844316200@qq.com>
2020-08-13 19:17:34 +0800

    添加promise实现

--- 总结/myPromise.js
+++ 总结/myPromise.js
@@ -13,7 +13,7 @@ function myPromise(fn) {
     if (that.status === PENDING) {
       that.status = FULFILLED;
       that.value = value;
-      that.fulfilledCallbacks.forEach(cb => cb(that.valuealue));
+      that.fulfilledCallbacks.forEach(cb => cb(that.value));
     }
   }
 
@@ -21,7 +21,31 @@ function myPromise(fn) {
     if (that.status === PENDING) {
       that.status = REJECTED;
       that.value = value;
-      that.rejectedCallbacks.forEach(cb => cb(that.valuealue))
+      that.rejectedCallbacks.forEach(cb => cb(that.value))
+    }
+  }
+
+  this.then = function(fulfilledCallback, rejectedCallback) {
+    fulfilledCallback = typeof fulfilledCallback === 'function'? fulfilledCallback: value => value;
+    rejectedCallback = typeof rejectedCallback === 'function'? rejectedCallback: error => { throw error }
+    
+    if (this.status === FULFILLED) {
+      return myPromise.resolve(this.value)
+    }
+    if (this.status === REJECTED) {
+      return myPromise.rejected(this.value)
+    }
+    if (this.status === PENDING) {
+      return new myPromise((resolve, rejected) => {
+        this.fulfilledCallbacks.push(() => {
+          const result = fulfilledCallback && fulfilledCallback(this.value) || null;
+          if (result instanceof myPromise) {
+            result.then(resolve)
+          } else {
+            resolve(result)
+          }
+        })
+      })
     }
   }
 
@@ -30,40 +54,190 @@ function myPromise(fn) {
   } catch (error) {
     reject(error);
   }
+}
 
-  this.then = function(onResolve, onRejected) {
-    if (that.status === PENDING) {
-      that.fulfilledCallbacks.push(() => onResolve(that.value));
-      that.rejectedCallbacks.push(() => onRejected(that.value))
+
+myPromise.all = function(arr) {
+  return new Promise((resolve, rejected) => {
+    let values = [];
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+      let promise = arr[i];
+      if (typeof promise.then === 'function') {
+        promise.then(res => {
+          // values.push(res);
+          console.log(res)
+          values[i] = res;
+          if (values.length === len) {
+            resolve(values);
+          }
+        })
+      } else {
+        // values.push(arr[i]);
+        values[i] = arr[i];
+        if (values.length === len) {
+          resolve(values);
+        }
+      }
     }
-    if (that.status === FULFILLED) {
-      onResolve(that.value)
+  })
+}
+
+myPromise.race = function(arr) {
+  return new Promise((resolve, rejected) => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+      let promise = arr[i];
+      if (typeof promise.then === 'function') {
+        promise.then(res => {
+          resolve(res)
+        })
+      } else {
+        resolve(arr[i])
+      }
     }
-    if (that.status === REJECTED) {
-      onRejected(that.value)
-    }
-  }
-
-  this.all = function(arr) {
-
-  }
-
+  })
 }
 
 //
-let p1 = new myPromise((resolve, rejected) => {
-  resolve('promise1234567890-=');
-});
+// let p1 = new myPromise((resolve, rejected) => {
+//   resolve('promise1234567890-=');
+// });
 
-console.log(p1.then(res => console.log(res)));
+// console.log(p1.then(res => console.log(res)));
 
 //制造异步场景
-new myPromise((resolved,rejected)=>{
+// new myPromise((resolved,rejected)=>{
+//   setTimeout(()=>{
+//       resolved(1)
+//   },0)
+// })
+// .then(value=>{
+//   console.log(value); // 1
+// })
+
+let a0 = (
+  function() {
+    return 'a0'
+  }
+)()
+
+let a1 = new myPromise((resolved,rejected)=>{
   setTimeout(()=>{
       resolved(1)
-  },0)
-}).then(value=>{
-  console.log(value); // 1
-  
+  },3000)
+});
+// a1.then(res => console.log('---a----', res));
+let a2 = new myPromise((resolved,rejected)=>{
+  setTimeout(()=>{
+      resolved(2)
+  },2)
+});
+let a3 = new myPromise((resolved,rejected)=>{
+  setTimeout(()=>{
+      resolved(3)
+  },3)
+});
+// Promise.all([a1, a2, a3]).then(res => {
+//   console.log(res)
+// })
+
+// myPromise.all([a1, a2, a3]).then(res => {
+//   console.log(res, 'myPromise')
+// })
+
+// myPromise.race([a1, a2, a3, a0]).then(res => {
+//   console.log(res, 'myPromise')
+// })
+
+
+// Promise.all([a1, a2, a3, a0]).then(res => {
+//   console.log(res)
+// })
+
+// Promise.race([a1, a2, a3, a0]).then(res => {
+//   console.log(res)
+// })
+
+
+function Promise1(fn) {
+  var state = 'pending';
+  var value;
+  var deferred;
+
+  function resolve(newValue) {
+    value = newValue;
+    state = 'resolved';
+
+    if(deferred) {
+      handle(deferred);
+    }
+  }
+
+  function handle(onResolved) {
+    if(state === 'pending') {
+      deferred = onResolved;
+      return;
+    }
+
+    onResolved(value);
+  }
+
+  this.then = function(onResolved) {
+    handle(onResolved);
+  };
+
+  fn(resolve);
+}
+
+let aa = new Promise1((resolved,rejected)=>{
+  setTimeout(()=>{
+      resolved(1)
+  },3000)
+});
+// aa.then(res => console.log('---a----', res));
+
+
+Promise1.all = function(arr) {
+  return new Promise1((resolve, rejected) => {
+    let values = [];
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+      let promise = arr[i];
+      if (typeof promise.then === 'function') {
+        promise.then(res => {
+          values[i] = res;
+          if (values.length === len) {
+            resolve(values);
+          }
+        })
+      } else {
+        values[i] = arr[i];
+        if (values.length === len) {
+          resolve(values);
+        }
+      }
+    }
+  })
+}
+
+Promise1.all([a1, a2, a3]).then(res => {
+  // console.log(res, 'Promise1--------')
+  console.log('=====')
 })
 
+
+Promise1.all([a1, a2, a3, a0]).then(res => {
+  console.log(res)
+})
+
+
+
+let a = new Promise((resove, rejected) => {
+  resove(1)
+}).then((r, j) => {
+  console.log(r);
+  return 111
+}).then(r => console.log(r))
+
+console.log(a)
---
+++ 总结/myPromise2.js
@@ -0,0 +1,213 @@
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+
+
+function myPromise(fn) {
+  let self = this;
+  self.state = PENDING;
+  self.value = undefined;
+  self.onResolvedCallback = [];
+  self.onRejectedCallback = [];
+
+  function resolved (value) {
+    if (self.state === PENDING) {
+      self.state = FULFILLED;
+      self.value = value;
+      // console.log('-resolved-222-----', self.value, self.state, self.onResolvedCallback)
+      self.onResolvedCallback.forEach(cb => cb(self.value))
+    }
+  }
+
+  function rejected (value) {
+    if (self.state === PENDING) {
+      self.state = REJECTED;
+      self.value = value;
+      self.onRejectedCallback.forEach(cb => cb(self.value))
+    }
+  }
+
+  fn(resolved, rejected);
+}
+
+myPromise.prototype.then = function(onResolved, onRejected) {
+  var self = this;
+  // var promise2;
+  
+  // 根据标准，如果then的参数不是function，则我们需要忽略它，此处以如下方式处理
+  onResolved = typeof onResolved === 'function' ? onResolved : function(value) { return value }
+  onRejected = typeof onRejected === 'function' ? onRejected : function(reason) { return reason }
+
+  if (self.state === FULFILLED) {
+    return new myPromise((resolved, rejected) => {
+      try {
+        let x = onResolved(self.value);
+        if (x instanceof myPromise) {
+          x.then(resolved, rejected)
+        }
+        resolved(x);
+      } catch (error) {
+        rejected(error);
+      }
+    })
+  }
+
+  if (self.state === REJECTED) {
+    return new myPromise((resolved, rejected) => {
+      try {
+        let x = onRejected(self.data)
+        if (x instanceof myPromise) {
+          x.then(resolved, rejected)
+        }
+      } catch (error) {
+        rejected(error);
+      }
+    })
+  }
+
+  if (self.state === PENDING) {
+    return new myPromise((resolved, rejected) => {
+      self.onResolvedCallback.push((value) => {
+        try {
+          let x = onResolved(value)
+          if (x instanceof myPromise) {
+            x.then(resolved, rejected);
+          }
+        } catch (error) {
+          rejected(error);
+        }
+      });
+      self.onRejectedCallback.push((reson) => {
+        try {
+          let x = onRejected(reson);
+          if (x instanceof myPromise) {
+            x.then(resolved, rejected);
+          }
+        } catch (error) {
+          rejected(error);
+        }
+      });
+
+    })
+  }
+  // return promise2;
+}
+
+myPromise.prototype.catch = function(onRejected) {
+  return this.then(null, onRejected)
+}
+
+myPromise.all1 = arr => {
+  let aResult = [];    //用于存放每次执行后返回结果
+  return new myPromise(function (resolve, reject) {
+    let i = 0;
+    next();    // 开始逐次执行数组中的函数(重要)
+    function next() {
+      arr[i].then(function (res) {
+        aResult.push(res);    // 存储每次得到的结果
+        i++;
+        if (i == arr.length) {    // 如果函数数组中的函数都执行完，便resolve
+          resolve(aResult);
+        } else {
+          next();
+        }
+      })
+    }
+  })
+};
+
+myPromise.all = function(arr) {
+  let values = [];
+  return new myPromise((resolved, rejected) => {
+    let i = 0;
+    next();
+    function next() {
+      let promise = arr[i];
+      if (typeof promise.then === 'function') {
+        promise.then(res => {
+          values[i] = res;
+          i++;
+          if (i === arr.length) {
+            resolved(values);
+          } else{
+            next();
+          }
+        })
+      } else {
+        values[i] = promise;
+        i++;
+        if (i === arr.length) {
+          resolved(i);
+        } else {
+          next();
+        }
+      }
+    }
+  })
+}
+
+myPromise.race = function(arr) {
+  return new myPromise((resolved, rejected) => {
+    for (let i = 0; i < arr.length; i++) {
+      let promise = arr[i];
+      if (typeof promise.then === 'function') {
+        promise.then(res => {
+          resolved(res);
+          return;
+        })
+      } else {
+        resolved(promise);
+        return;
+      }
+    }
+  })
+}
+
+// let a1 = new myPromise((resolved,rejected)=>{
+//   setTimeout(()=>{
+//       resolved(111);
+//   },3000)
+// });
+
+// a1.then(res => console.log('----', res));
+
+// new myPromise(resolve => resolve(8))
+//   .then()
+//   .then()
+//   .then(value => {
+//     console.log('====', value)
+//   })
+
+
+let a1 = new myPromise((resolved,rejected)=>{
+  setTimeout(()=>{
+      resolved(1)
+  },300)
+});
+// a1.then(res => console.log('---a----', res));
+let a2 = new myPromise((resolved,rejected)=>{
+  setTimeout(()=>{
+      resolved(2)
+  },20)
+});
+let a3 = new myPromise((resolved,rejected)=>{
+  setTimeout(()=>{
+      resolved(3)
+  },0)
+});
+let a4 = new myPromise((resolved,rejected)=>{
+  setTimeout(()=>{
+      resolved(4)
+  },30)
+});
+let a5 = new myPromise(resolve => resolve(8))
+  .then()
+  .then()
+  
+myPromise.all([a1, a2, a3, a4, a5]).then(res => {
+  console.log(res);
+})
+
+myPromise.race([a1, a2, a3, a4, a5]).then(res => {
+  console.log(res);
+})
