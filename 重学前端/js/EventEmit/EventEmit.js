927e96ff5fac5070577e704252259f5bce2683fc
HMenen <844316200@qq.com>
2020-12-31 14:11:18 +0800

    add

---
+++ 重学前端/js/EventEmit/EventEmit.js
@@ -0,0 +1,28 @@
+class EventEmeitter {
+  constructor() {
+    this._events = this._events || new Map(); // 储存事件/回调键值对
+    this._maxListeners = this._maxListeners || 10; // 设立监听上限
+  }
+}
+
+
+// 触发名为type的事件
+EventEmeitter.prototype.emit = function(type, ...args) {
+  let handler;
+  // 从储存事件键值对的this._events中获取对应事件回调函数
+  handler = this._events.get(type);
+  if (args.length > 0) {
+    handler.apply(this, args);
+  } else {
+    handler.call(this);
+  }
+  return true;
+};
+
+// 监听名为type的事件
+EventEmeitter.prototype.addListener = function(type, fn) {
+  // 将type事件以及对应的fn函数放入this._events中储存
+  if (!this._events.get(type)) {
+    this._events.set(type, fn);
+  }
+};
---
+++ 重学前端/js/deepClone/deepClone.js
@@ -0,0 +1,73 @@
+function deepClone1(obj) {
+  let ret = obj;
+  if (obj && typeof obj === 'object') {
+    Array.isArray(obj) ? ret = []: ret = {};
+    Object.keys(obj).forEach(key => {
+      ret[key] = deepClone1(obj[key]);
+    })
+  }
+  return ret;
+}
+
+
+function getType(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1);
+}
+
+function cloneRegExp(data) {
+  const pattern = data.valueOf();
+  let flag = '';
+  flag += pattern.global? 'g': '';
+  flag += pattern.ignoreCase? 'i': '';
+  flag += pattern.multiline? 'm': '';
+  return new RegExp(pattern.source, flag);
+}
+/**
+ * deepClone增强版
+ * @param {Any}} obj 
+ */
+function deepClone2(obj) {
+  let ret = obj;
+  if (obj === null || typeof obj !== 'object'){
+    return obj;
+  }
+  const objType = getType(obj)
+  switch(objType) {
+    case 'Function':
+      return obj;
+    case 'Date':
+      return new Date(obj.getTime());
+    case 'Array':
+      ret = [];
+      break;
+    case 'RegExp':
+      ret = cloneRegExp(obj);
+      break;
+    default:
+      // if (obj) {
+      //   ret = Object.create(Object.getPrototypeOf(obj));
+      // }
+      ret = {};
+      break;
+  }
+
+  Object.keys(obj).forEach(key => {
+    ret[key] = deepClone2(obj[key]);
+  })
+  return ret;
+}
+
+// 测试==================================
+var o1 = {
+  a: 111,
+  b: [1, 2, 3],
+  c: new Date(),
+  d: function a() {console.log('----')},
+  e: null,
+  f: {a: 1},
+  g: /^1/mi
+}
+var d1 = deepClone2(o1);
+d1.a = 121;
+console.log('---o1--', o1)
+console.log('---d1--', d1)
