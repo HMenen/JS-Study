691d5765c578388a2a85901c09d70237d16f5380
HMenen <844316200@qq.com>
2021-01-24 23:03:56 +0800

    add

---
+++ 重学前端/真题/腾讯/交集.js
@@ -0,0 +1,55 @@
+/**
+ * 350  给定两个数组，编写一个函数来计算它们的交集。
+示例 1：
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2,2]
+示例 2:
+输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出：[4,9]
+
+说明：
+输出结果中每个元素出现的次数，应与元素在两个数组中出现次数的最小值一致。
+我们可以不考虑输出结果的顺序。
+进阶：
+如果给定的数组已经排好序呢？你将如何优化你的算法？
+如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+如果 nums2 的元素存储在磁盘上，内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+ /**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+  let map1 = {}, retMap = {}, ret = [];
+  for (let i = 0; i < nums1.length; i++) {
+    const char = nums1[i];
+    map1[char] ? map1[char]++ : map1[char] = 1;
+  }
+  for (let i = 0; i < nums2.length; i++) {
+    const char = nums2[i];
+    if (map1[char]) {
+      if (retMap[char]) {
+        if (retMap[char] < map1[char]) {
+          retMap[char]++
+        }
+      } else {
+        retMap[char] = 1;
+      }
+    }
+  }
+  Object.keys(retMap).forEach(c => {
+    const arr = new Array(retMap[c]).fill(+c);
+    ret = [...ret, ...arr];
+  })
+  console.log(ret);
+  return ret
+};
+
+
+console.log('----------', intersect([4,9,5], [9,4,9,8,4]));
