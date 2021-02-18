f344ebc84245381e6a4467c9107fb3cfe723d807
HMenen <844316200@qq.com>
2021-02-18 16:39:41 +0800

    add

--- 重学前端/js/bind/bind.js
+++ 重学前端/js/bind/bind.js
@@ -1,10 +1,48 @@
 /**
- * 
+ * https://juejin.cn/post/6844903718089916429
  * 1、bind是Function原型链中的Function.prototype的一个属性，它是一个函数，修改this指向，合并参数传递给原函数，返回值是一个新的函数。
  * 2、bind返回的函数可以通过new调用，这时提供的this的参数被忽略，指向了new生成的全新对象。内部模拟实现了new操作符。
  * 3、es5-shim源码模拟实现bind时用Function实现了length。
  * 事实上，平时其实很少需要使用自己实现的投入到生成环境中。但面试官通过这个面试题能考察很多知识。比如this指向，原型链，闭包，函数等知识，可以扩展很多。
+ * 
+ * 
+ * 实现bind要做什么
+ * 返回一个函数，绑定this，传递预置参数
+ * bind返回的函数可以作为构造函数使用。故作为构造函数时应使得this失效，但是传入的参数依然有效
+ * 
  */
+
+// 最终版 删除注释 详细注释版请看上文
+Function.prototype.bind = Function.prototype.bind || function bind(thisArg){
+  if(typeof this !== 'function'){
+      throw new TypeError(this + ' must be a function');
+  }
+  var self = this;
+  var args = [].slice.call(arguments, 1);
+  var bound = function(){
+      var boundArgs = [].slice.call(arguments);
+      var finalArgs = args.concat(boundArgs);
+      if(this instanceof bound){
+          if(self.prototype){
+              function Empty(){}
+              Empty.prototype = self.prototype;
+              bound.prototype = new Empty();
+          }
+          var result = self.apply(this, finalArgs);
+          var isObject = typeof result === 'object' && result !== null;
+          var isFunction = typeof result === 'function';
+          if(isObject || isFunction){
+              return result;
+          }
+          return this;
+      }
+      else{
+          return self.apply(thisArg, finalArgs);
+      }
+  };
+  return bound;
+}
+
 Function.prototype.myBind = function() {
     const self = [].shift.call(arguments);
     let args = [...arguments];
@@ -40,17 +78,17 @@ Function.prototype.myBind3 = function() {
 
 //=================测试
 //1. new测试， original不发回值
-var obj = {
-  name: 'hmm',
-};
-function original(a, b){
-  console.log('this', this); // original {}
-  console.log('typeof this', typeof this); // object
-  this.name = b;
-  console.log('name', this.name); // 2
-  console.log('this', this);  // original {name: 2}
-  console.log([a, b]); // 1, 2
-}
+// var obj = {
+//   name: 'hmm',
+// };
+// function original(a, b){
+//   console.log('this', this); // original {}
+//   console.log('typeof this', typeof this); // object
+//   this.name = b;
+//   console.log('name', this.name); // 2
+//   console.log('this', this);  // original {name: 2}
+//   console.log([a, b]); // 1, 2
+// }
 
 // var bFun = original.myBind3(obj, 1);
 // var b = new bFun(2);
@@ -58,31 +96,31 @@ function original(a, b){
 
 
 //2. 
-var obj2 = {
-  name: 'hmm',
-};
-function original2(a, b){
-  console.log('this', this); // original {}
-  console.log('typeof this', typeof this); // object
-  this.name = b;
-  console.log('name', this.name); // 2
-  console.log('this', this);  // original {name: 2}
-  console.log([a, b]); // 1, 2
-  return {name: 'hahahha', age: 18}
-}
+// var obj2 = {
+//   name: 'hmm',
+// };
+// function original2(a, b){
+//   console.log('this', this); // original {}
+//   console.log('typeof this', typeof this); // object
+//   this.name = b;
+//   console.log('name', this.name); // 2
+//   console.log('this', this);  // original {name: 2}
+//   console.log([a, b]); // 1, 2
+//   return {name: 'hahahha', age: 18}
+// }
 
-var func1 = original2.myBind3(obj2, 1);
-var b2 = new func1(2);
-console.log('-------b--', b2)
-console.log('---11111----', b2.__proto__ === original2.prototype); //false
+// var func1 = original2.myBind3(obj2, 1);
+// var b2 = new func1(2);
+// console.log('-------b--', b2)
+// console.log('---11111----', b2.__proto__ === original2.prototype); //false
 
-//3. 一般函数调用测试
-function demo(age, a1) {
-  console.log(this.name, '-----', age, a1)
-}
-var a = {
-  name: 'hahaha'
-}
+// //3. 一般函数调用测试
+// function demo(age, a1) {
+//   console.log(this.name, '-----', age, a1)
+// }
+// var a = {
+//   name: 'hahaha'
+// }
 // const d = demo.myBind3(a);
 // d(18, 'lll');
 
@@ -111,6 +149,28 @@ Function.prototype.myBind2 = function() {
 }
 
 
+Function.prototype.bindTest = function(thisArg) {
+  const self = this;
+  let args = [].slice.call(arguments, 1);
+  function bound() {
+    let boundArgs = [].slice.call(arguments);
+    const finalArgs = [...args, ...boundArgs];
+    console.log('---111----', this)
+    if (this instanceof bound) {
+      if (self.prototype) {
+        function Empty() {};
+        Empty.prototype = self.prototype;
+        bound.prototype = new Empty();
+      }
+      const ret = self.apply(this, finalArgs);
+      return typeof ret === ('function' || 'object') ?  ret: this;
+    } else {
+      return self.apply(thisArg, finalArgs)
+    }
+  }
+  return bound;
+}
+
 // function demo(age, a1) {
 //   console.log(this.name, '-----', age, a1)
 // }
@@ -122,23 +182,107 @@ Function.prototype.myBind2 = function() {
 // // d(18, 'lll');
 
 
+
+
 // var obj = {
 //   name: '若川',
 // };
 // function original(a, b){
-//   console.log('this', this); // original {}
-//   console.log('typeof this', typeof this); // object
+//   // console.log('this', this); // original {}
+//   // console.log('typeof this', typeof this); // object
 //   this.name = b;
-//   console.log('name', this.name); // 2
+//   // console.log('name', this.name); // 2
 //   console.log('this', this);  // original {name: 2}
 //   console.log([a, b]); // 1, 2
 // }
-// var bound = original.myBind2(obj, 1);
+// var bound = original.bindTest(obj, 1);
 // // var bound = original.bind(obj, 1);
 // var newBoundResult = new bound(2);
-// // console.log(newBoundResult, 'newBoundResult'); // original {name: 2}
+// console.log(newBoundResult, 'newBoundResult'); // original {name: 2}
 // console.log('=========-------------', newBoundResult.__proto__ === original.prototype)  //true
 // console.log('=========-------------', bound.prototype)
 // console.log('=========-------------', newBoundResult.prototype)
 
 
+
+// var obj = {
+//   name: '若川',
+// };
+
+// function demo(age, a1) {
+//   console.log(this.name, '-----', this, age, a1)
+// }
+
+// var a = demo.bind(obj, 1);
+
+
+// Function.prototype.bindTest = function(thisArg) {
+//   const self = this;
+//   let args = [].slice.call(arguments, 1);
+//   function bound() {
+//     let boundArgs = [].slice.call(arguments);
+//     const finalArgs = [...args, ...boundArgs];
+//     console.log('---111----', this)
+//     if (this instanceof bound) {
+//       if (self.prototype) {
+//         function Empty() {};
+//         Empty.prototype = self.prototype;
+//         bound.prototype = new Empty();
+//       }
+//       const ret = self.apply(this, finalArgs);
+//       return typeof ret === ('function' || 'object') ?  ret: this;
+//     } else {
+//       return self.apply(thisArg, finalArgs)
+//     }
+//   }
+//   return bound;
+// }
+
+Function.prototype.bindAAA = function() {
+  const func = this;
+  const _this = arguments[0];
+  let originArgs = [].slice.call(arguments, 1);
+  const bound = function() {
+    let args = [].slice.call(arguments);
+    const finalArgs = [...originArgs, ...args];
+    // console.log('-thisthisthis111------', this instanceof bound, func.prototype)
+    if (this instanceof bound) {
+      if (func.prototype) {
+        function F() {};
+        F.prototype = func.prototype;
+        bound.prototype = new F();
+        // this.__proto__ = func.prototype;
+      }
+      const ret = func.apply(this, finalArgs);
+      if (typeof ret === 'function' || (typeof ret === 'object' && ret !== null)) {
+        return ret;
+      } else {
+        console.log('-thisthisthis222------', this instanceof bound, func.prototype)
+        return this;
+      }
+    } else {
+      return func.apply(_this, finalArgs);
+    }
+  }
+  return bound;
+}
+
+var obj = {
+  name: '1若川',
+};
+function original(a, b){
+  console.log('this', this); // original {}
+  console.log('typeof this', typeof this); // object
+  this.name = b;
+  console.log('name', this.name); // 2
+  console.log('this', this);  // original {name: 2}
+  console.log([a, b]); // 1, 2
+}
+var bound = original.bindAAA(obj, 1);
+// bound(111);
+// var bound = original.bind(obj, 1);
+var newBoundResult = new bound(2);
+// console.log(newBoundResult, 'newBoundResult'); // original {name: 2}
+// console.log('=========-------------', newBoundResult.__proto__, newBoundResult.__proto__ === original.prototype)  //true
+console.log('=========-------------', bound.prototype)
+// console.log('=========-------------', newBoundResult.prototype)
--- 重学前端/js/bind/call.js
+++ 重学前端/js/bind/call.js
@@ -9,7 +9,7 @@ Function.prototype.myCall = function() {
   let originFn = thisArg[args];
   const isHasOriginFun = thisArg.hasOwnProperty(__fn);
   thisArg[__fn] = this;
-  const ret = thisArg[__fn](...args);
+  const ret = thisArg[__fn](args);
   delete thisArg[__fn];
   if (isHasOriginFun) {
     thisArg[__fn] = originFn;
@@ -33,10 +33,51 @@ Function.prototype.myApply = function() {
 }
 
 
+// var doSth = function (name, age){
+//   var type = Object.prototype.toString.call(this);
+//   console.log(typeof doSth);
+//   console.log(this === firstArg);
+//   console.log('type:', type);
+//   console.log('this:', this.name);
+//   console.log('args:', [name, age], arguments);
+//   return 'this--';
+// };
+
+// var name = 'window';
+
+// var student = {
+//   name: '若川',
+//   age: 18,
+//   doSth: 'doSth',
+//   __fn: 'doSth',
+// };
+// var firstArg = student;
+// var result = doSth.myApply(firstArg, [1, {name: 'Rowboat'}]);
+// // var result2 = doSth.myCall(firstArg, 1, {name: 'Rowboat'});
+// console.log('result:', result);
+// console.log('student.doSth:', student.doSth);
+// // console.log('result2:', result2);
+
+Function.prototype.callAAA = function() {
+  const func = this;
+  const context = arguments[0];
+  let args = arguments[1];
+  let __fn = '__' + new Date().getTime();
+  const isHasOriginFun = context.hasOwnProperty(__fn);
+  let originFn = context[__fn];
+  context[__fn] = func;
+  const ret = context[__fn](args);
+  delete context[__fn];
+  if (isHasOriginFun) {
+    context[__fn] = originFn;
+  }
+  return ret;
+}
+
 var doSth = function (name, age){
   var type = Object.prototype.toString.call(this);
   console.log(typeof doSth);
-  console.log(this === firstArg);
+  console.log(this === student);
   console.log('type:', type);
   console.log('this:', this.name);
   console.log('args:', [name, age], arguments);
@@ -51,9 +92,9 @@ var student = {
   doSth: 'doSth',
   __fn: 'doSth',
 };
-var firstArg = student;
-var result = doSth.myApply(firstArg, [1, {name: 'Rowboat'}]);
-// var result2 = doSth.myCall(firstArg, 1, {name: 'Rowboat'});
+// var result = doSth.callAAA(student, [1, {name: 'Rowboat'}], 121212);
+// var result = doSth.call(student, 1, {name: 'Rowboat'}, 121212);
+var result = doSth.callAAA(student, 1, {name: 'Rowboat'}, 121212);
 console.log('result:', result);
 console.log('student.doSth:', student.doSth);
 // console.log('result2:', result2);
